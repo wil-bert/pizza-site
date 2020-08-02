e68caacd17d195c0b52f623dd3a7e0cdad178dbe
wil-bert <ianwilbuts@gmail.com>
2020-08-03 01:37:18 +0300

    Add delivery alert

---
+++ js/script.js
@@ -0,0 +1,13 @@
+
+
+$(document).ready(function () {
+    $("#order").submit(function () {
+
+        var name = $("input#name").val();
+        var tel = $("input#tel").val();
+        var location = $("input#location").val();
+
+        alert("Hi " + name + ". Your order will be delivered to " +  location + " within one hour.The delivery will cost ksh 200/= Thank you for ordering!");
+    });
+
+});
